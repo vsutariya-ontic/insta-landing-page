13435809900d7077a087cc9030f126fe5fef2d75
Vastal Sutariya <vastalsutariya@Admins-MacBook-Pro.local>
2024-01-05 12:51:58 +0530

    first commit

---
+++ helper-functions.js
@@ -0,0 +1,110 @@
+const suggestedUserList = [
+    {
+        username: "vicky_323",
+        mutualFollowers: 24
+    },
+    {
+        username: "vijay_777",
+        mutualFollowers: 17
+    },
+    {
+        username: "alex_123",
+        mutualFollowers: 10
+    },
+    {
+        username: "sara_456",
+        mutualFollowers: 32
+    },
+    {
+        username: "lisa_789",
+        mutualFollowers: 5
+    },
+];
+const storyUserList = [
+    {
+        username: 'john_doe123',
+        name: 'John Doe'
+    },
+    {
+        username: 'emily_789',
+        name: 'Emily Johnson'
+    },
+    {
+        username: 'alex_456',
+        name: 'Alex Smith'
+    },
+    {
+        username: 'sara_654',
+        name: 'Sara Davis'
+    },
+    {
+        username: 'mike_1122',
+        name: 'Mike Thompson'
+    },
+    {
+        username: 'lisa_321',
+        name: 'Lisa Williams'
+    },
+    {
+        username: 'chris_987',
+        name: 'Chris Brown'
+    },
+    {
+        username: 'olivia_456',
+        name: 'Olivia White'
+    },
+    {
+        username: 'david_789',
+        name: 'David Robinson'
+    },
+    {
+        username: 'julia_456',
+        name: 'Julia Turner'
+    },
+    {
+        username: 'ryan_321',
+        name: 'Ryan Johnson'
+    },
+    {
+        username: 'nancy_789',
+        name: 'Nancy Miller'
+    },
+    {
+        username: 'sam_1122',
+        name: 'Samuel Davis'
+    },
+    {
+        username: 'amy_321',
+        name: 'Amy Clark'
+    },
+    {
+        username: 'peter_987',
+        name: 'Peter Walker'
+    },
+    {
+        username: 'mia_456',
+        name: 'Mia Turner'
+    },
+    {
+        username: 'eric_789',
+        name: 'Eric Anderson'
+    },
+    {
+        username: 'grace_1122',
+        name: 'Grace White'
+    },
+    {
+        username: 'jason_321',
+        name: 'Jason Carter'
+    },
+];
+
+export const generateStoryData = () => {
+    // run some api calls
+    return storyUserList;
+}
+
+export const generateSuggestedUsers = () => {
+    // run some api calls
+    return suggestedUserList;
+}
---
+++ script.js
@@ -0,0 +1,62 @@
+const doc = document.getElementsByTagName('body');
+
+// const suggestionObject = document.getElementById('suggestion-demo');
+
+// const suggestionList = document.getElementById('suggestion-list');
+
+// suggestionList.appendChild(suggestionObject);
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const suggestionObject = document.getElementById('suggestion-demo');
+//     const suggestionList = document.getElementById('suggestion-list');
+//     suggestionList.appendChild();
+// });
+
+// <ul id="list-id">
+//     <li id="item-id">First item</li>
+// </ul>
+
+// const list = document.getElementById('list-id');
+// const item = document.getElementById('item-id');
+// const newItem = item.cloneNode(true);
+// newItem.textContent = 'Second Item';
+// list.appendChild(newItem);
+// console.log(list);
+// console.log(item);
+// list.append(item);
+// console.log('added')
+
+import { generateStoryData, generateSuggestedUsers } from "/helper-functions.js";
+const suggestedUserList = generateSuggestedUsers();
+const storyUserList = generateStoryData();
+
+const loadSuggestions = (toBeRenderedList) => {
+    const suggestionList = document.getElementById('suggestion-list');
+    const suggestionDemo = document.getElementById('suggestion-demo');
+
+    toBeRenderedList.forEach((suggestedUser) => {
+        const newSuggestedUser = suggestionDemo.cloneNode(true);
+        const userContent = newSuggestedUser.querySelector('#username');
+        const mutualFollowers = newSuggestedUser.querySelector('#mutual-followers');
+        userContent.textContent = suggestedUser.username;
+        mutualFollowers.textContent = `${suggestedUser.mutualFollowers} mutual followers`;
+        suggestionList.appendChild(newSuggestedUser);
+    });
+    suggestionDemo.remove();
+};
+
+const loadStories = (toBeRenderedStories) => {
+    const storyList = document.getElementById('story-list');
+    const storyDemo = document.getElementById('story-demo');
+    
+    toBeRenderedStories.forEach(element => {
+        const newStoryItem = storyDemo.cloneNode(true);
+        const usernameContent = newStoryItem.querySelector('#username');
+        usernameContent.textContent = element.username;
+        storyList.appendChild(newStoryItem);
+    });
+    storyDemo.remove();
+};
+
+loadSuggestions(suggestedUserList);
+loadStories(storyUserList);
